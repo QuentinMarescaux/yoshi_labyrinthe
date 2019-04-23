691c42577018594a827ed510ad684a0cb6094098
Quentin Marescaux <quentin.marescaux98@gmail.com>
2019-04-23 19:58:00 +0200

    add files

---
+++ constantes.py
@@ -0,0 +1,25 @@
+"""Constantes du jeu de Labyrinthe Yoshi"""
+
+#Paramètres de la fenêtre
+LARGEUR_ACCUEIL = 450
+HAUTEUR_ACCUEIL = 450
+HAUTEUR_BANDEAU = 50
+
+NOMBRE_SPRITE = [10, 15, 20, 25]
+TAILLE_SPRITE = 30
+
+#Gestion des dossiers
+FOLDER_IMAGES = "images"
+FOLDER_NIVEAUX = "niveaux"
+FOLDER_SCORES = "meilleurs_scores"
+
+#Personnalisation de la fenêtre
+TITRE_FENETRE = "Yoshi Labyrinthe"
+IMAGE_ICONE = FOLDER_IMAGES + "/icon.png"
+
+#Listes des images du jeu
+IMAGE_ACCUEIL = FOLDER_IMAGES + "/accueil.png"
+IMAGE_FOND = FOLDER_IMAGES + "/fond.jpg"
+IMAGE_MUR = FOLDER_IMAGES + "/mur.png"
+IMAGE_DEPART = FOLDER_IMAGES + "/depart.png"
+IMAGE_ARRIVEE = FOLDER_IMAGES + "/arrivee.png"
